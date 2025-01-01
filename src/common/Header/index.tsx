8deeaf75b3d11199feafbbeb4a3827bb2b3612ab
DABIN JEONG <41@b41.kr>
2025-01-01 17:55:45 +0900

    style: adjust logo size

--- src/common/Header/index.tsx
+++ src/common/Header/index.tsx
@@ -9,9 +9,16 @@ const Header = () => {
     <header className="relative mb-[50px] flex w-full flex-col">
       <div
         id="header-image"
-        className="animate-gradient flex h-[270px] w-full justify-center rounded-b-md border-x-2 border-b-2 border-primary-dark bg-gradient-to-r from-[#F4C430] via-[#FFB90F] to-[#DAA520] align-middle"
+        className="animate-gradient flex h-[270px] w-full items-start rounded-b-md border-x-2 border-b-2 border-primary-dark bg-gradient-to-r from-[#F4C430] via-[#FFB90F] to-[#DAA520] p-8"
       >
-        <Image src="/logo.png" alt="blog41" width={200} height={200} className="object-contain" />
+        <Image
+          id="logo"
+          src="/logo.png"
+          alt="blog41"
+          width={120}
+          height={120}
+          className="object-contain"
+        />
       </div>
       <div className="absolute bottom-[-20px] left-1/2 -translate-x-1/2">
         <div className="flex gap-4">{MENU_ITEMS.map(MenuItem)}</div>
