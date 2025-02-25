161e8cde99c037b3b6e4d773310c0444df7041fb
DABIN JEONG <41@b41.kr>
2025-02-26 02:05:16 +0900

    feat: add til page & api

---
+++ src/app/api/til/route.ts
@@ -0,0 +1,37 @@
+import { TIL_TAG } from '@/common/constants';
+import { MongoClient } from 'mongodb';
+import { NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
+
+export async function GET(request: NextRequest) {
+  try {
+    const client = await MongoClient.connect(process.env.NEXT_PUBLIC_DB_URL as string, {
+      auth: {
+        username: process.env.NEXT_PUBLIC_DB_ID as string,
+        password: process.env.NEXT_PUBLIC_DB_PASSWORD as string
+      }
+    });
+
+    const db = client.db(process.env.NEXT_PUBLIC_DB_KEY);
+    
+    // "TIL" 태그가 있는 게시물만 필터링하는 쿼리 조건
+    const query = { tags: TIL_TAG };
+    
+    // TIL 태그가 있는 모든 게시물 가져오기 (페이지네이션 없음)
+    const posts = await db.collection('posts')
+      .find(query)
+      .sort({ createdAt: -1 })
+      .toArray();
+
+    await client.close();
+    
+    return NextResponse.json({
+      posts,
+      totalPosts: posts.length
+    });
+
+  } catch (error) {
+    console.error('데이터베이스 에러:', error);
+    return NextResponse.json({ message: '서버 에러가 발생했습니다.' }, { status: 500 });
+  }
+} 
--- src/common/constants.ts
+++ src/common/constants.ts
@@ -7,6 +7,11 @@ export const MENU_ITEMS: MenuItemType[] = [
     href: '/',
   },
   {
+    key: 'til',
+    title: 'TIL',
+    href: '/til'
+  },
+  {
     key: 'portfolio',
     title: '포트폴리오',
     href: '/portfolio',
@@ -22,3 +27,4 @@ export const POSTS_PER_PAGE = 10;
 export const PAGINATION_DISPLAY_COUNT = 5;
 export const RECENT_POSTS_COUNT = 5;
 export const DEFAULT_DATE_FORMAT = 'YYYY.MM.DD';
+export const TIL_TAG = 'TIL';
