2701f12f69173e3a73a87ce8abc75069ce7cb29d
DABIN JEONG <41@b41.kr>
2025-02-26 01:48:22 +0900

    fix: remove fullscreen overlay to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
-import FullscreenOverlay from '@/common/FullscreenOverlay';
 import Home from '@/components/Home';
-import HomeOverlay from '@/components/HomeOverlay';
 import Tags from '@/components/Tags';
 
 export default async function HomePage() {
 
   return (
     <>
-      <FullscreenOverlay>
-        <HomeOverlay />
-      </FullscreenOverlay>
       <Tags />
       <Home />
     </>
