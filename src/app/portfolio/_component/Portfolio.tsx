122f3910141013fc35bbd2942c0b13f92ae68d47
DABIN JEONG <41@b41.kr>
2025-04-06 03:14:56 +0900

    fix: modify portfolio page p color
    - 검정색으로 고정
    - 이메일 추가 (연락하기 -> 포트폴리오로 이동)

--- src/app/portfolio/_component/Portfolio.tsx
+++ src/app/portfolio/_component/Portfolio.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Portfolio = () => {
   return (
     <div className="border-normal flex w-full flex-col items-center justify-center gap-4 border-primary-dark bg-white p-4">
-      <p>준비 중입니다.</p>
+      <p className="text-2xl text-gray-800">준비 중입니다.</p>
+      <a href="mailto:malgolil41@gmail.com" className="text-blue-500 hover:text-blue-700">
+          E-Mail : malgolil41@gmail.com
+        </a>
     </div>
   );
 };
